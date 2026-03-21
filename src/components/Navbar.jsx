b43107b3f8695493d5396041154a7e6873f0faba
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-03-21 19:47:05 +0530

    feat: update Navbar component with backdrop blur and add new image asset
    - Enhanced Navbar styling by adding a backdrop blur effect for better visibility.
    - Introduced a new image asset (download.jpg) to the project for future use.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center ">
+    <div className="flex justify-center backdrop-blur-4xl ">
             <ul className="flex gap-9 w-fit px-7 py-4 justify-center mt-10 bg-transparent rounded-4xl border-2 border-[gray] border-solid fixed">
                 <li>Home</li>
                 <li>About</li>
